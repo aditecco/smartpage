d3cd09124fa316b8c61df38266a9ba3a3c32c609
Alessandro Di Tecco <alessandro@ditecco.me>
2021-10-30 12:47:31 +0200

    Created app prototype

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="input-container" v-if="toggleModal">
+    <form action="#">
+      <input type="text" @change="handleChange" />
+
+      <button type="submit" @click.prevent="handleSubmit">Add</button>
+    </form>
+  </div>
+
+  <ul>
+    <li v-for="(item, i) in items" :key="i">{{ item.title }}</li>
+  </ul>
+
+  <button type="button" @click="handleClick">Add an item</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      toggleModal: false,
+      input: "",
+      items: [],
+    };
+  },
+  methods: {
+    handleClick(e) {
+      console.log("click ", e);
+      this.toggleModal = !this.toggleModal;
+    },
+    handleChange(e) {
+      console.log("change ", e);
+      this.input += e.currentTarget.value;
+    },
+    handleSubmit(e) {
+      console.log("submit ", e);
+      this.items.push({ title: this.input });
+      this.input = "";
+      this.toggleModal = false;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +52,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.input-container {
+  padding: 2rem 1rem;
+  margin: 1rem;
+  border-radius: 4px;
+  border: 1px solid #777;
+}
+
+button {
+  box-sizing: border-box;
+  border: none;
+  padding: 20px;
+  line-height: 20px;
+  border-radius: 10px;
+  background: whitesmoke;
+  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.15);
 }
 </style>
