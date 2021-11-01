6f28b24ae9380e611c150023c7f2b776742b4560
Alessandro Di Tecco <alessandro@ditecco.me>
2021-11-01 19:45:15 +0100

    Added utils, constants

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+/* ---------------------------------
+Constants
+--------------------------------- */
+
+export const URL_FILTER = /https?:\/\/(?:www\.)?/;
+export const STORAGE_DATA_KEY = "SMARTPAGE_ITEMS";
---
+++ src/utils/index.js
@@ -0,0 +1,31 @@
+/* ---------------------------------
+Utils
+--------------------------------- */
+
+/**
+ * Creates bookmark descriptions out of URL slugs (WIP)
+ * (Imported from Bookman)
+ */
+
+import { URL_FILTER } from "@/constants";
+
+export function slugToDesc(url) {
+  let desc;
+
+  if (url.charAt(url.length - 1) === "/") {
+    url = url.substr(0, url.length - 1);
+  }
+
+  desc = url
+    .replace(URL_FILTER, "")
+    .split("/")
+    .pop()
+    .split("-")
+    .map((w) => w.charAt(0).toUpperCase() + w.substring(1))
+    .join(" ")
+    .replace(".html", "")
+    .replace("#", "")
+    .replace(/[\d]*/, "");
+
+  return desc || url;
+}
