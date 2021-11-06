0175a47770662dd3b883d7ef494aba2537bdc8c8
Alessandro Di Tecco <alessandro@ditecco.me>
2021-11-06 21:35:08 +0100

    feature/components | Created Container.vue

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container" :style="{ maxWidth: mw || '800px' }">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+  props: ["mw"],
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  padding: 0 1rem;
+  margin: 0 auto;
+}
+</style>
