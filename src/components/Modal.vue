a44c9b9bd9557a7e7910ce393e505f37a85dd70f
Alessandro Di Tecco <alessandro@ditecco.me>
2021-11-07 00:18:50 +0100

    feature/components | Created Modal.vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="input-modal" v-if="show">
+    <button class="input-modal-close-button" @click="onClose">
+      <i class="material-icons">close</i>
+    </button>
+
+    <Container>
+      <slot />
+    </Container>
+  </div>
+</template>
+
+<script>
+import Container from "@/components/Container";
+
+export default {
+  name: "Modal",
+  props: ["show", "onClose"],
+  components: { Container },
+};
+</script>
+
+<style scoped></style>
