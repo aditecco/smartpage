ee19c70bf266352207e8749530ea34a9eae55c35
Alessandro Di Tecco <alessandro@ditecco.me>
2021-11-06 21:16:45 +0100

    feature/components | Created Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <div class="container">
+      <slot />
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<!-- TODO a styles not working -->
+<style scoped>
+footer {
+  padding: 1rem;
+  background: whitesmoke;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+  padding: 2px 4px;
+  font-size: small;
+  text-transform: uppercase;
+  border-bottom: 2px solid transparent;
+}
+
+a:hover {
+  color: black;
+  border-bottom: 2px solid black;
+}
+</style>
