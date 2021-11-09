41c9267a355588b721fe66efff82bfb91b80c36c
Alessandro Di Tecco <alessandro@ditecco.me>
2021-11-09 23:53:34 +0100

    feature/serverless | Updated constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,5 @@ Constants
 
 export const URL_FILTER = /https?:\/\/(?:www\.)?/;
 export const STORAGE_DATA_KEY = "SMARTPAGE_CARDS";
+export const APP_URL = "http://localhost:9999/";
+export const LAMBDA_LOCATION = ".netlify/functions/";
